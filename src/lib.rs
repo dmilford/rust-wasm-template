43558bce0aeaac4f04a15bf92384d947c22ee3fb
Doug Milford <51060011+dmilford@users.noreply.github.com>
2020-01-06 14:14:04 -0800

    Adding lib

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+
+#[wasm_bindgen]
+pub fn say_howdy() {
+    log("Howdy Coby!");
+}
